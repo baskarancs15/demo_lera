b338258a09892925e2aaf4c909b978f34fb1f6ba
Baskaran <baskaran.k@cintanatech.com>
2021-01-08 14:54:32 +0530

    dashboardcommit

--- src/app/default/default.component.ts
+++ src/app/default/default.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-default',
@@ -10,6 +11,58 @@ export class DefaultComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // var ctx = <HTMLCanvasElement>document.getElementById('myChart').getContext('2d');
+    const canvas = <HTMLCanvasElement> document.getElementById('myChart');
+    const ctx = canvas.getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'pie',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
+const canvas2 = <HTMLCanvasElement> document.getElementById('mybarChart');
+    const ctx2 = canvas2.getContext('2d');
+var stackedBar = new Chart(ctx2, {
+  type: 'bar',
+  // data: [20, 10],
+  // The data for our dataset
+  data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45]
+    }]
+},
+  options: {
+      scales: {
+          xAxes: [{
+              stacked: true
+          }],
+          yAxes: [{
+              stacked: true
+          }]
+      }
   }
+});
+
+
+  }
+
+
 
 }
