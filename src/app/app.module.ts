19d78fdd8bc0319ea2a7e46fd9a611054e546bb7
Kanimozhiharish18 <57133205+Kanimozhiharish18@users.noreply.github.com>
2021-01-08 07:32:59 +0530

    registration component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  exports: [
+    RegistrationComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
